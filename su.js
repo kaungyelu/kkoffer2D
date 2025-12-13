9f94cbd14f488ea5308359e10d26793938656176
kaungyelu <kaung2016.ye@gmail.com>
2025-12-13 19:57:00 +0630

    Update su.js

--- su.js
+++ su.js
@@ -1,4 +1,4 @@
  // Supabase Configuration
-        const SUPABASE_URL = 'https://stzepyuibjtoohofafue.supabase.co';
-        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0emVweXVpYmp0b29ob2ZhZnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNzQ5NzUsImV4cCI6MjA4MDk1MDk3NX0.Tgj1sJfBGQP9KbK5NzahlBoFU9121osc_FEvTsr47eQ';
+        const SUPABASE_URL = 'https://uegrqmyswaxffiejxnoi.supabase.co';
+        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlZ3JxbXlzd2F4ZmZpZWp4bm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2MjU2NjYsImV4cCI6MjA4MTIwMTY2Nn0.OaYtJvafd4b70KbnveJtTHX_8mpsUbfwZkaCZoQrEx4';
            
